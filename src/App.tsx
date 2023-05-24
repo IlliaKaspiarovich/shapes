f6eff10e792f9519d836ae7bfab581b1e987ab1e
Illia.Kaspiarovich <Illia.Kaspiarovich@easyfundraising.org.uk>
2023-05-24 23:57:37 +0200

    adds layer styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react'
 import { Controls } from 'components/Controls'
+import styles from './App.module.css'
 
 export const App: FC = () => {
   return (
-    <div>
+    <div className={styles.appContainer}>
       <Controls />
     </div>
   )
--- src/components/Controls/index.tsx
+++ src/components/Controls/index.tsx
@@ -3,11 +3,11 @@ import { FC } from 'react'
 export const Controls: FC = () => {
 
   return (
-    <>
+    <div>
       Project ID:&nbsp;
       <input type='text' placeholder='For random leave empty'/>
       <button>Fetch</button>
-      <hr />
-    </>
+      <hr style={{ marginTop: 10, marginBottom: 10 }} />
+    </div>
   )
 }
